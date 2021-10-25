f5c1e210bcea15b37b97838b8716c1878757e2cc
Pianist16 <47276367+Pianist16@users.noreply.github.com>
2021-10-25 11:59:12 +0200

    Add files via upload

--- round_robin.py
+++ round_robin.py
@@ -7,19 +7,19 @@ Created on Thu Oct 21 11:04:21 2021
 
 #algorithm description: https://nrich.maths.org/1443
 
-#players = ['raphael', 'michelangelo', 'donatello', 'leonardo']
-players = ['7', '1', '2', '3', '4', '5', '6', '8']
+players = ['raphael', 'michelangelo', 'donatello', 'leonardo']
+#players = ['7', '1', '2', '3', '4', '5', '6', '8']
 
 
-def split_list(list):
-    half = len(list)//2
-    return list[:half], list[half:]
+def split_list(my_list):
+    half = len(my_list)//2
+    return my_list[:half], my_list[half:]
 
-def final_pairs_from_even(list):
+def final_pairs_from_even(my_list):
     final_pairs = []
-    for j in range(len(list)-1):
+    for j in range(len(my_list)-1):
         
-        players_1, players_2 = split_list(list)
+        players_1, players_2 = split_list(my_list)
         
         # first pair
         pairs = [players_1[-1] + " - " + players_2[-1]]
@@ -36,18 +36,18 @@ def final_pairs_from_even(list):
         
         final_pairs.append(pairs)
         
-        list.insert(0, list.pop(players.index(list[-2])))
+        my_list.insert(0, my_list.pop(players.index(my_list[-2])))
         
     return final_pairs
 
 
 
 
-def final_pairs_from_odd(list):
+def final_pairs_from_odd(my_list):
     final_pairs = []
-    for j in range(len(list)):
+    for j in range(len(my_list)):
         
-        players_1, players_2 = split_list(list)
+        players_1, players_2 = split_list(my_list)
         players_2 = players_2[1:]
         players_2.reverse()
         
@@ -61,7 +61,7 @@ def final_pairs_from_odd(list):
         
         final_pairs.append(pairs)
         
-        list.insert(0, list.pop())
+        my_list.insert(0, my_list.pop())
         
     return final_pairs
 
